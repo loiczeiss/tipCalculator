84d67ab2b243b204d1c5971365600549f6f4d18d
Loïc Zeiss <loiczeiss@gmail.com>
2023-10-12 16:20:39 +0200

    basic layout done, css features to do

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import dollar from "./assets/icon-dollar.svg";
+import personLogo from './assets/icon-person.svg'
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+const [tipAmount, setTipAmount] = useState('$0.00')
+const [tipTotal, setTipTotal] = useState('$0.00')
+const [divStyle, setDivStyle] = useState({ backgroundColor: "hsl(189, 41%, 97%)" })
+const InputClicked = () => {
+  setDivStyle({ backgroundColor: "hsl(189, 41%, 97%)", border:'2px solid hsl(172, 67%, 45%) '})
+}
+
   return (
     <>
       <div className="whole">
@@ -23,16 +32,51 @@ function App() {
             <div className="tip">
               <p className="tipTxt">Select Tip %</p>
               <div className="flex-container">
-  <div className="item">5%</div>
-  <div className="item">10%</div>
-  <div className="item">15%</div>
-  <div className="item">20%</div>
-  <div className="item">25%</div>
-  <div className="item" style={{backgroundColor: 'hsl(189, 41%, 97%)'}}><input type="text" /></div>
-</div>
+                <div className="item">5%</div>
+                <div className="item">10%</div>
+                <div className="item">15%</div>
+                <div className="item">25%</div>
+                <div className="item">50%</div>
+                <div
+                  className="item"
+                  id="itemCustom"
+                  style={divStyle}
+                >
+                  <input type="text" placeholder="Custom" onFocus={InputClicked}/>
+                </div>
+              </div>
+            </div>
+       
+              <p style={{color:'hsl(186, 14%, 43%)', marginTop:'30px' }}>Number of People</p>
+              <div className="person">
+              <img src={personLogo} alt="personSign" id="personSign" />
+              <input
+                type="text"
+                name="personTotal"
+                id="personTotal"
+                placeholder="0"
+              />
             </div>
           </section>
-          <section></section>
+          <section className="calculWindow">
+         <div>   <div className="tipWindows">
+              <div >
+                <p style={{color: 'hsl(0, 0%, 100%)'}}>Tip Amount</p>
+                <p style={{color: 'hsl(186, 14%, 43%)'}}>/ person</p>
+              </div>
+              <p style={{color: 'hsl(172, 67%, 45%)', fontSize: '24px'}}>{tipAmount}</p>
+            </div>
+            <div  className="tipWindows">
+            <div>
+                <p style={{color: 'hsl(0, 0%, 100%)'}}>Total</p>
+                <p style={{color: 'hsl(186, 14%, 43%)'}}>/ person</p>
+              </div>
+              <p style={{color: 'hsl(172, 67%, 45%)', fontSize: '24px'}}>{tipTotal}</p>
+            </div></div>
+            <button style={{backgroundColor: 'hsl(172, 67%, 45%)', marginTop: '10px',
+          marginBottom:'5px'}}><p>RESET</p></button>
+
+          </section>
         </article>
       </div>
     </>
